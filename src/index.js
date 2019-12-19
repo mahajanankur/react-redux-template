ec304d5c9638b1f91f79be4041457762d760a7af
Ankur-Mahajan <ankur.mahajan@grazitti.com>
2019-12-19 18:23:06 +0530

    Added enhancers for redux dev tool to work

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import reducers from './reducers';
@@ -12,8 +12,14 @@ import ReduxPromise from 'redux-promise';
 //Error: Actions must be plain objects. Use custom middleware for async actions.
 import thunk from 'redux-thunk';
 
+// Getting Redux DevTools To Work - https://stackoverflow.com/questions/37298559/getting-redux-devtools-to-work
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 const store = createStore(
     reducers,
+    enhancers,
     applyMiddleware(logger, ReduxPromise, thunk)
 );
 
