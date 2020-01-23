c02a976e36f3737332e9c7e77419102ddf92f530
Ankur-Mahajan <ankur.mahajan@grazitti.com>
2020-01-23 22:57:42 +0530

    Added ec2 context

--- src/constants/application.js
+++ src/constants/application.js
@@ -1,2 +1,3 @@
-exports.BASE_CONTEXT = "http://127.0.0.1:3333/api";
+// exports.BASE_CONTEXT = "http://127.0.0.1:3333/api";
+exports.BASE_CONTEXT = "http://ec2-13-235-24-170.ap-south-1.compute.amazonaws.com:3333/api";
 exports.PAGINATION_SIZE = 5;
