cb95293562034e8ac4809ea6fe8b4d01717bb968
Ankur-Mahajan <ankur.mahajan@grazitti.com>
2020-01-23 23:45:50 +0530

    updated config

--- src/constants/application.js
+++ src/constants/application.js
@@ -1,3 +1,2 @@
-// exports.BASE_CONTEXT = "http://127.0.0.1:3333/api";
-exports.BASE_CONTEXT = "http://ec2-13-235-24-170.ap-south-1.compute.amazonaws.com:3333/api";
+exports.BASE_CONTEXT = "http://127.0.0.1:3333/api";
 exports.PAGINATION_SIZE = 5;
